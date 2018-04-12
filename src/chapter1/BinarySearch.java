5b4696049e601bb5d8edd81c717c1a8bc4277a18
nagiosleaner <pxz6603@126.com>
2018-04-12 22:26:11 +0800

    测试测试

--- src/chapter1/BinarySearch.java
+++ src/chapter1/BinarySearch.java
@@ -1,10 +1,16 @@
 package chapter1;
 
+import java.util.Scanner;
+
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			
+		}
+		sc.close();
 	}
 
 }
