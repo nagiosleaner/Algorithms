42cec6b835c81437e9abf43a7fc198e6af54126f
RangerPang <pxz6603@126.com>
2018-05-12 21:08:20 +0800

    new stage

---
+++ src/stackqueue/TwoStack.java
@@ -0,0 +1,16 @@
+package stackqueue;
+
+import java.util.Scanner;
+
+public class TwoStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			
+		}
+		sc.close();
+	}
+
+}
